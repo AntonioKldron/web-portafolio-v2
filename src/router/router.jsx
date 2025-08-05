c7bd4e15301ef55373d9ae5f089f6778ea501394
Jose Cornelio <jcornelio@intelisis.com.mx>
2025-08-05 08:36:37 -0600

    hotfix

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,36 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Index from "../page/index.jsx";
-import IntelisisSection from "../page/intelisis.jsx";
-import ItmSection from '../page/fasItmMorelia.jsx'
 
 export const router = createBrowserRouter([
-  {
-    path: "/index",
-    children: [
-      {
-        index: true,
-        element: <Index />,
-      },
-    ],
-  },
-  {
-    path: "/intelisis",
-    children: [
-      {
-        index: true,
-        element: <IntelisisSection />,
-      },
-    ],
-  },
-  {
-    path: "/itm",
-    children: [
-      {
-        index: true,
-        element: <ItmSection />,
-      },
-    ],
-  },
   {
     path: "*",
     element: <Index />,
