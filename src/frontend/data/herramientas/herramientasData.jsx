e918fed25bbe06eac50c397341889ee30e204b5f
Jose Antonio Cornelio Calderon <jcornelio@cinepolis.com>
2026-03-24 09:20:03 -0600

    hotfix

--- src/frontend/data/herramientas/herramientasData.jsx
+++ src/frontend/data/herramientas/herramientasData.jsx
@@ -27,7 +27,7 @@ export const herramientasData = {
       { id: "cat-2", title: "Backend", items: [dt.nestjs, dt.fastapi, dt.django, dt.djangorest] },
       { id: "cat-3", title: "Frontend", items: [dt.html, dt.css, dt.astro, dt.react, dt.tailwind, dt.bootstrap] },
       { id: "cat-4", title: "ERP Solutions", items: [dt.peoplesoft, dt.intelisis] },
-      { id: "cat-5", title: "Bases de Datos", items: [dt.sqlserver, dt.oracle, dt.postgresql, dt.mysql, dt.supabase] },
+      { id: "cat-5", title: "Bases de Datos", items: [dt.sqlserver, dt.oracle, dt.postgresql, dt.mysql, dt.supabase, dt.firebase] },
       { id: "cat-6", title: "Infraestructura", items: [dt.docker, dt.linux, dt.windows, dt.vercel] },
       { id: "cat-7", title: "Dev Tools", items: [dt.terminal, dt.vstudio, dt.powerbi ,dt.vscode, dt.cursor, dt.jetbrains, dt.git, dt.gitlab, dt.github, dt.trello, dt.figma, dt.termius] }
     ]
@@ -43,7 +43,7 @@ export const herramientasData = {
       { id: "cat-2", title: "Backend", items: [dt.nestjs, dt.fastapi, dt.django, dt.djangorest] },
       { id: "cat-3", title: "Frontend", items: [dt.html, dt.css, dt.astro, dt.react, dt.tailwind, dt.bootstrap] },
       { id: "cat-4", title: "ERP Solutions", items: [dt.peoplesoft, dt.intelisis] },
-      { id: "cat-5", title: "Databases", items: [dt.sqlserver, dt.oracle, dt.postgresql, dt.mysql, dt.supabase] },
+      { id: "cat-5", title: "Databases", items: [dt.sqlserver, dt.oracle, dt.postgresql, dt.mysql, dt.supabase, dt.firebase] },
       { id: "cat-6", title: "Infrastructure", items: [dt.docker, dt.linux, dt.windows, dt.vercel] },
       { id: "cat-7", title: "Dev Tools", items: [dt.terminal, dt.vstudio, dt.powerbi, dt.vscode, dt.cursor, dt.jetbrains, dt.git, dt.gitlab, dt.github, dt.trello, dt.figma, dt.termius] }
     ]
